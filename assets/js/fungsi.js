78114cff687ef099c3d85414e5431e1835eaa333
IlhamRusli <118285647+IlhamRusli@users.noreply.github.com>
2022-11-15 22:02:41 +0700

    Update fungsi.js

--- assets/js/fungsi.js
+++ assets/js/fungsi.js
@@ -7,7 +7,7 @@ var dataBarang = [{
 function showBarang() {
   var listBarang = document.getElementById("table");
 
-  listBarang.innerHTML = "<tr><th>Nama Aksesoris</th><th>Jumlah</th><th>Total</th><th>Action</th></tr>";
+  listBarang.innerHTML = "<tr><th>Nama Barang</th><th>Jumlah</th><th>Total</th><th>Action</th></tr>";
 
   for (let i = 0; i < dataBarang.length; i++) {
     if (i !== 0) {
